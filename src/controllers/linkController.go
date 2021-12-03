beec6e29b3ed8ec2e7b0c45899f347963be93c8b
Terry Tay <txrrythk@gmail.com>
2021-12-04 03:13:43 +0800

    links

--- src/controllers/linkController.go
+++ src/controllers/linkController.go
@@ -20,5 +20,13 @@ func Link(c echo.Context) error {
 
 	database.DB.Where("user_id = ?", id).Find(&links)
 
+	for i, link := range links {
+		var orders []models.Order
+
+		database.DB.Where("code = ? and complete = true", link.Code).Find(&orders)
+
+		links[i].Orders = orders
+	}
+
 	return c.JSON(http.StatusOK, links)
 }
